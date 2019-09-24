923c7d011e9ab7428184abe163fdf79d669490b4
TJ Jiang <tjiang@tubi.tv>
2019-09-23 17:18:47 -0700

    modify filters, additional comments

--- ip_soft_match_table.sql
+++ ip_soft_match_table.sql
@@ -1,7 +1,3 @@
-/*
-This is the query that actually writes the table; should live in github, not a console scratch
-*/
-
 drop table if exists scratch.ip_soft_match;
 
 create table scratch.ip_soft_match
@@ -17,7 +13,8 @@ The latest timestamp of a device and IP pairing are also tallied
 WITH device_ip_activity AS (
   SELECT device_id,
          case when client_ip ilike '%.%' then client_ip
-            else client_ipv6
+              when client_ip ilike '%:%' and client_ipv6 is not NULL and len(client_ipv6)>20 then client_ipv6
+              else NULL
          end as ip,
          /*
          production solution should join with user_alias_first_country_v
@@ -30,50 +27,60 @@ WITH device_ip_activity AS (
   and u.signup_countries='US'
   and device_id not in (0, '')
   and device_id is not NULL
---   and ip is not NULL
---   and ip<>''
+  and nvl(client_ip, client_ipv6) is not NULL
+  and nvl(client_ip, client_ipv6) not in ('', ' ')
   /*
   Filter below is only enabled for debugging purposes
   */
 --   WHERE s.ts > dateadd('day', -3, current_date)
   GROUP BY 1,2
+  having ip is not NULL
 )
 /*
 rankings aggregates by device_id via the rank() window function, and orders IP activeness using
 the number of server impressions first and recency second (to break ties in case of equal
 activity on multiple IPs)
+
+In other words, the rankings CTE helps to assign a primary (or a set of primary) IP addresses to each
+deviceid based on activity.
+
 ip_rank represents the activity level of a device on all the IPs it has pinged the ad server from
 E.g. my iphone has pinged the ad server 100 times at home, and 50 times on tubi wifi. Home would be ranked
 1 and Tubi-wifi would be ranked 2
 */
+
 , rankings as (
   SELECT device_id,
   ip,
   RANK() OVER (partition by device_id order by activity desc, latest_ts desc) as ip_rank
   FROM device_ip_activity
 )
+
 /*
 device_per_ip tallies the number of devices that were active on the same IP
-There is no specific time period involved, as the cutoff was chosen offline
+If an IP has too many devices, it is excluded as the dataset would become far too noisy otherwise
+
+Todo: Once the probabilistic model has been developed, we may be able to loosen this constraint
 */
+
 , devices_per_ip as (
   select ip, count(distinct device_id) as num_devices
   from rankings
-  where ip_rank = 1
   group by ip
   having num_devices<=5
 )
+
 /*
-ip_device_pairs is really only a filter step
+ip_device_pairs is a filter step that excludes the ips filtered out in devices_per_ip
 */
 , ip_device_pairs as (
   select r.ip, r.device_id
   from rankings r
-  join (select ip from devices_per_ip) d
+  join (select ip from devices_per_ip) d -- remove from dataset the ips that have too many devices active on them
   on r.ip=d.ip
---   where ip_rank=1
-  where ip_rank<=10
+  where ip_rank<=10 -- only include ips that rank at least top 10 on a given device
 ),
+
 /*
 activity_ts tallies the first and last activities of a particular device on a particular IP
 device_active_order is the order at which devices pinged a particular IP
